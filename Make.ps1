0e626c19a8299626e5becb86c8d2757bec04749e
Giorgos Petrou <85218116+gpetrounrt@users.noreply.github.com>
2021-11-22 17:12:07 +0200

    Add coverage action in Make.ps1 (#8)

--- Make.ps1
+++ Make.ps1
@@ -2,7 +2,7 @@
 [CmdletBinding()]
 Param(
     [Parameter(Position = 1)]
-    [ValidateSet("clean", "build", "unit-test", "help")]
+    [ValidateSet("clean", "build", "unit-test", "coverage", "help")]
     [string] $action = "")
 
 # Establish and enforce coding rules in expressions, scripts and script blocks.
@@ -10,6 +10,10 @@ Set-StrictMode -Version Latest
 $errorActionPreference = "Stop"
 
 Set-Variable SolutionPath -Option Constant -Value "$PSScriptRoot\BuildLogReporter.sln" -Force -ErrorAction SilentlyContinue
+Set-Variable TestCoveragePath -Option Constant -Value "$PSScriptRoot\tests\coverage" -Force -ErrorAction SilentlyContinue
+Set-Variable TestCoverageResultsPath -Option Constant -Value "$TestCoveragePath\results" -Force -ErrorAction SilentlyContinue
+Set-Variable TestSummaryPath -Option Constant -Value "$PSScriptRoot\tests\summary" -Force -ErrorAction SilentlyContinue
+Set-Variable ToolsPath -Option Constant -Value "$PSScriptRoot\tools" -Force -ErrorAction SilentlyContinue
 
 function DeleteDirectories([string] $directoryName) {
     $directories = Get-ChildItem $PSScriptRoot $directoryName -Recurse -Directory
@@ -19,11 +23,38 @@ function DeleteDirectories([string] $directoryName) {
     }
 }
 
+function DeleteDirectory([string] $directoryPath) {
+    if (Test-Path $directoryPath) {
+        Remove-Item $directoryPath -Force -Recurse
+    }
+}
+
 function Clean() {
     & dotnet clean $SolutionPath --configuration Release
 
     DeleteDirectories "bin"
     DeleteDirectories "obj"
+
+    DeleteDirectory $TestSummaryPath
+    DeleteDirectory $TestCoveragePath
+}
+
+function CreateCoverageReport() {
+    $isReportGeneratorInstalled = $false
+    if (Test-Path $ToolsPath) {
+        $tools = dotnet tool list --tool-path $ToolsPath
+        $isReportGeneratorInstalled = $tools -Like '*dotnet-reportgenerator-globaltool*'
+    }
+
+    if (!$isReportGeneratorInstalled) {
+        dotnet tool install --tool-path $ToolsPath dotnet-reportgenerator-globaltool
+    }
+
+    & "$ToolsPath\reportgenerator.exe" `
+        -reports:"$TestCoverageResultsPath\**\*.xml" `
+        -targetdir:"$TestCoveragePath\report" `
+        -reporttypes:"Badges;Html;MarkdownSummary" `
+        -title:"Build Log Reporter"
 }
 
 function DisplayHelp() {
@@ -49,7 +80,16 @@ switch -wildcard ($action) {
     }
 
     "unit-test" {
-        & dotnet test $SolutionPath --configuration Release --no-build
+        & dotnet test --configuration Release `
+            --no-build `
+            --collect:"XPlat Code Coverage" `
+            --logger "junit;LogFilePath=$TestSummaryPath/{assembly}.xml" `
+            --results-directory "$TestCoverageResultsPath"
+        break
+    }
+
+    "coverage" {
+        CreateCoverageReport
         break
     }
 
